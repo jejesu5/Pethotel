f34a71100dcb02e486a0cba0b6758a61a5b756cd
Jesus Arenas <arenasjesus.w@gmail.com>
2023-02-02 22:50:26 -0500

    hotfix route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 const { Router } = require('express')
 const router = Router()
 const userRoutes = require('./user.routes')
+const reservationRoutes = require('./reservations.routes')
 
 router.use('/user', userRoutes)
 
-router.use('/reservation', require('./reservation.routes'))
+router.use('/reservation', reservationRoutes)
 
 module.exports = router
